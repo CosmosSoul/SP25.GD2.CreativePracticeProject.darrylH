d29c7f62ce6bee7a98f11f1028098e2a75650c6a
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2025-02-25 18:00:48 -0500

    Extended Floor and added walls to create room, added movement script for WASD movement and mouse looking

---
+++ Template Project/Assets/Scripts/visitorMovementScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class visitorMovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        transform.Translate(movement * Time.deltaTime * 5f);
+
+        // Get mouse movement for rotation
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        
+        // Rotate the player based on mouse X movement
+        transform.Rotate(Vector3.up * mouseX * 2f);
+    }
+}
